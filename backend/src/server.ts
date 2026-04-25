bbca17a80429491b8bf6f1a4db4519a0a60574cc
Malhar Degaonkar <97455728+captainmmd1304@users.noreply.github.com>
2026-04-25 08:57:18 +0530

    server.ts changed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,4 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-const server = app.listen(PORT, () => {
-  console.log(`🚀 [Server]: Running at http://localhost:${PORT}`);
-});
-
-process.on('SIGTERM', () => {
-  console.log('SIGTERM signal received: closing HTTP server');
-  server.close(() => {
-    console.log('HTTP server closed');
-  });
-});
+export default app;
